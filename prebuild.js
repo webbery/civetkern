57c64ab53e7af593fef72f914a774736e26c8686
webbery <webbery@foxmail.com>
2022-08-12 00:20:58 +0800

    ci

--- prebuild.js
+++ prebuild.js
@@ -2,9 +2,22 @@ console.info("┏ Build Dependencies")
 console.info("")
 console.info("  downloading gqlite")
 var process = require('child_process');
-function runCommand(hookScript) {
+function runCommand(hookScript, workingDirectory) {
   console.log(`${hookScript}`);
-  return process.execSync(hookScript, {stdio: [this.stdin, this.stdout, this.stderr], encoding: 'buffer'});
+  return process.execSync(hookScript, {stdio: [this.stdin, this.stdout, this.stderr], encoding: 'buffer', cwd: workingDirectory});
 }
-let error = runCommand('git clone --recursive https://github.com/webbery/gqlite.git gqlite')
-error = runCommand('cmake -B gqlite/build -DCMAKE_BUILD_TYPE=Release')
+let error
+let fs = require('fs')
+let os = require('os')
+if (os.platform() === "win32") {
+  if (fs.existsSync('gqlite')) {
+    runCommand('cmake -B build -DCMAKE_BUILD_TYPE=Release -DGQLITE_BUILD_SHARED=TRUE -DGQLITE_BUILD_TEST=FALSE gqlite', 'gqlite')
+    runCommand('cmake --build build --config Release', 'gqlite')
+  }
+  
+} else {
+  if (fs.existsSync('gqlite')) {
+    runCommand('cmake -B gqlite/build -DCMAKE_BUILD_TYPE=Release -DGQLITE_BUILD_SHARED=TRUE -DGQLITE_BUILD_TEST=FALSE gqlite', '.')
+    runCommand('cmake --build gqlite/build --config Release', '.')
+  }
+}
