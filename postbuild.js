e9f063bfa458ddb31fd4f2a65791bd33be12c332
webbery <webbery@foxmail.com>
2022-08-13 18:49:40 +0800

    ci

---
+++ postbuild.js
@@ -0,0 +1,17 @@
+var process = require('child_process');
+function runCommand(hookScript, workingDirectory) {
+  console.log(`${hookScript}`);
+  return process.execSync(hookScript, {stdio: [this.stdin, this.stdout, this.stderr], encoding: 'buffer', cwd: workingDirectory});
+}
+
+let fs = require('fs')
+let os = require('os')
+if (os.platform() === "win32") {
+  runCommand('cp gqlite/build/Release/gqlite.dll build/Release/', '.')
+}
+else if (os.platform() === "linux") {
+  runCommand('cp gqlite/build/Release/libgqlite.so build/Release/', '.')
+}
+else if (os.platform() === "darwin") {
+  runCommand('cp gqlite/build/Release/gqlite.dyld build/Release/', '.')
+}
